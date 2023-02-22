fd550b1100a4bd3754728e4993f97663f7be5c43
Evan Ryan <evantryan@gmail.com>
2023-02-21 18:23:46 -0800

    getting camera mapping addon started

---
+++ __init__.py
@@ -0,0 +1,18 @@
+bl_info = {
+    "name": "CameraProjectionMaterial",
+    "author": "Evan Ryan",
+    "version": (0, 0, 0),
+    "blender": (3, 3, 0),
+    "location": "Node Editor",
+    "category": "Node",
+    "description": "Camera projection mapping material node",
+    "warning": ""
+}
+
+VERSION = 'v0.0.0' 
+
+def register():
+    pass
+
+def unregister():
+    pass
