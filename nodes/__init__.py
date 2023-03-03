cb62795efe7d78ea980b4170ce8fd9b6a29f9308
Evan Ryan <evantryan@gmail.com>
2023-03-02 22:29:00 -0800

    ref for tomorrow

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -3,8 +3,11 @@ from .camera_mapping import CameraMappingShaderNode
 from nodeitems_utils import NodeItem, register_node_categories, unregister_node_categories
 from nodeitems_builtins import ShaderNodeCategory, CompositorNodeCategory
 
+# could maybe use https://github.com/Secrop/ShaderNodesExtra2.80/blob/master/__init__.py
+# to put these into an existing menu
+
 node_categories = [
-    ShaderNodeCategory("MAPPING_NODES", "Mapping Nodes", items=[
+    ShaderNodeCategory("PIPELINENODES", "Mapping Nodes", items=[
         NodeItem("CameraMappingShaderNode"),
     ]),
 ]
@@ -18,10 +21,10 @@ def register():
     for cls in classes:
         register_class(cls)
 
-    register_node_categories("SH_TESTING_NODES", node_categories)
+    register_node_categories("PIPELINENODES", node_categories)
 
 def unregister():
-    unregister_node_categories("SH_TESTING_NODES")
+    unregister_node_categories("PIPELINENODES")
     from bpy.utils import unregister_class
     for cls in reversed(classes):
         unregister_class(cls)
