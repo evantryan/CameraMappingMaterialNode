aaca572d011eae1637aad757c60f9f3869fdbd71
Evan Ryan <evantryan@gmail.com>
2023-03-03 20:50:02 -0800

    testing

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -7,11 +7,11 @@ from nodeitems_builtins import ShaderNodeCategory, CompositorNodeCategory
 # could maybe use https://github.com/Secrop/ShaderNodesExtra2.80/blob/master/__init__.py
 # to put these into an existing menu. fingers crossed   
 
-# node_categories = [
-#     ShaderNodeCategory("PIPELINENODES", "Mapping Nodes", items=[
-#         NodeItem("CameraMappingShaderNode"),
-#     ]),
-# ]
+node_categories = [
+    ShaderNodeCategory("CUSTOM_SHADER_NODES", "Mapping Nodes", items=[
+        NodeItem("CameraMappingShaderNode"),
+    ]),
+]
 
 classes = [
     CameraMappingShaderNode
@@ -22,12 +22,12 @@ def register():
     for cls in classes:
         register_class(cls)
 
-    #register_node_categories("PIPELINENODES", node_categories)
-    node_menu_include('PIPELINE_NODES', "Pipeline Nodes", CameraMappingShaderNode)
+    register_node_categories("CUSTOM_SHADER_NODES", node_categories)
+    #node_menu_include('PIPELINE_NODES', "Pipeline Nodes", CameraMappingShaderNode)
 
 def unregister():
-    #unregister_node_categories("PIPELINENODES")
-    node_menu_exclude('PIPELINE_NODES', "Pipeline Nodes", CameraMappingShaderNode)
+    unregister_node_categories("CUSTOM_SHADER_NODES")
+    #node_menu_exclude('PIPELINE_NODES', "Pipeline Nodes", CameraMappingShaderNode)
 
     from bpy.utils import unregister_class
     for cls in reversed(classes):
